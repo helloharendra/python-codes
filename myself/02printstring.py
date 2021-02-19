041223f65135fced75eaa1442003d7c404137e13
harendraprajapati <harendraprajapati72@gmail.com>
2021-02-19 13:57:14 +0530

    mysecondcommit

---
+++ myself/02printstring.py
@@ -0,0 +1,5 @@
+'''this is commentes'''
+#this is single line comment
+print('''hi guys this is harendra prajapati 
+         belonging from azamgarh
+         want to learn python''')
---
+++ myself/03.playsound.py
@@ -0,0 +1,3 @@
+from playsound import playsound
+
+playsound ('//Users//harendraprajapati//Desktop/python//play.mp3')
