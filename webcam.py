5ab0e24729b7ae0075026040db5e325745b06f60
harendraprajapati72 <harendraprajapati72@gmail.com>
2021-03-02 13:50:35 +0530

    list

--- webcam.py
+++ webcam.py
@@ -4,6 +4,8 @@ webcam = cv2.VideoCapture(0)
 
 while True:
     State, img = webcam.read()
+    print(State)
+    print(img)
     if State:
         cv2.imshow("mycam",img)
         if cv2.waitKey(1) == ord('q'):
