f730988ed850d09088d1ab7bd7be47c4bde33fc7
Harendra Prajapati <harendraprajapati72@gmail.com>
2022-05-25 00:00:51 +0530

    streamline

---
+++ first.py
@@ -0,0 +1,9 @@
+from email.mime import image
+import streamlit as st
+st.sidebar.header('Program By: Harendra')
+st.title('Use of streamlit')
+st.image('3.png')
+st.info('Here I am using streamlit it is very easy for frontend to use , for a python(Data Science) Developer  ')
+
+st.slider('give me rating')
+st.button('submit')
